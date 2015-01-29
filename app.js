66f4eb86a7960a0c8dcf19a7ad2d51a50e17378f
Charlie Tilt <charlie@rescour.com>
2015-01-28 20:48:43 -0500

    added app, config and package

---
+++ app.js
@@ -0,0 +1,7 @@
+var ghost = require('ghost');
+var path = require('path');
+ghost({
+  config: path.join(__dirname, 'config.js')
+}).then(function (ghostServer) {
+  ghostServer.start();
+});
---
+++ config.js
@@ -0,0 +1,62 @@
+// # Ghost Configuration
+// Setup your Ghost install for various environments
+// Documentation can be found at http://support.ghost.org/config/
+
+var path = require('path'),
+    config;
+
+config = {
+  production: {
+    url: 'http://browntoilet.herokuapp.com',
+    database: {
+      client: 'pg',
+      connection: {
+        host: process.env.POSTGRES_HOST || 'localhost',
+        user: process.env.POSTGRES_USER || 'postgres',
+        password: process.env.POSTGRES_PASS || 'postgres',
+        database: process.env.POSTGRES_DB || 'ghost',
+        port: process.env.POSTGRES_PORT || 5432
+      },
+      debug: false
+    },
+    server: {
+      host: '0.0.0.0',
+      port: process.env.PORT || 2368
+    },
+    paths: {
+        contentPath: path.join(__dirname, '/content/')
+    }
+  },
+
+  development: {
+    url: 'http://localhost:2368',
+    // database: {
+    //     client: 'sqlite3',
+    //     connection: {
+    //         filename: path.join(__dirname, '/content/data/ghost-dev.db')
+    //     },
+    //     debug: false
+    // },
+    // database: {
+    //   client: 'pg',
+    //   connection: {
+    //     host: process.env.POSTGRES_HOST || 'localhost',
+    //     user: process.env.POSTGRES_USER || 'postgres',
+    //     password: process.env.POSTGRES_PASS || 'postgres',
+    //     database: process.env.POSTGRES_DB || 'ghost',
+    //     port: process.env.POSTGRES_PORT || 5432
+    //   },
+    //   debug: false
+    // },
+    server: {
+      host: '0.0.0.0',
+      port: process.env.PORT || 2368
+    },
+    paths: {
+        contentPath: path.join(__dirname, '/content/')
+    }
+  }
+};
+
+// Export config
+module.exports = config;
