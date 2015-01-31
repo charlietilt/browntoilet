99cca57f434b69e8592b27529f257a197d12c548
Charlie Tilt <charlie@rescour.com>
2015-01-31 12:48:56 -0500

    added new relic to the app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var ghost = require('ghost');
 var path = require('path');
+var newrelic = require('newrelic');
 ghost({
   config: path.join(__dirname, 'config.js')
 }).then(function (ghostServer) {
